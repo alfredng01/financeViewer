373e0f4d3febadf77cc073c548fbaacf75d629db
alfred <alfredng019@gmail.com>
2023-05-18 08:18:57 -0400

    Updated Expense Item Component

--- starter-code/src/components/ExpenseItem.js
+++ starter-code/src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 
 export default function ExpenseItem()
 {
+    const itemDate = new Date(2021, 5, 15);
+    const itemTitle = 'Bills';
+    const itemAmount = 284.12;
+
     return (
-        <h1>
-            Expense Item:
-        </h1>
+        <div className="expense-item">
+            <div>
+                {itemDate.toISOString()}
+            </div>
+            <div className="expense-item_description">
+                <h2>{itemTitle}</h2>
+                <div className="expense-item_price">{itemAmount}</div>
+            </div>
+        </div>
     );
 }
